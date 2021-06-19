021c8f52cc556653cc0dda7cb2f6be6e84459629
rkun123 <tren111213@gmail.com>
2021-06-19 02:40:49 +0000

    [add] WIP: PostDetail

---
+++ pages/posts/_id.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+		hogefuga
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component({})
+export default class PostDetail extends Vue {
+  
+}
+
+</script>
+
+<style>
+
+</style>
+
