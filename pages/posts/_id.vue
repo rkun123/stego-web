b8d65e838be6fd412bdb64ef80c4a4b4361239cd
soh <izumisohpublic@gmail.com>
2021-06-19 13:43:04 +0900

    データを表示

--- pages/posts/_id.vue
+++ pages/posts/_id.vue
@@ -1,12 +1,37 @@
 <template>
   <div>
-		{{ post.content }}
+
+
+	<h2>Details</h2>
+		<p>temperature : 	</p>
+		<p>velocity : {{post.velocity}}	</p>
+		<p>direction : {{post.direction}}</p>
+		<p>elevation : {{post.elevation}}</p>
+		<p>latlng : {{post.lat}}/{{post.lng}}</p>
+
+	<h2>posted Data</h2>
+		<p>	{{post.user.created_at}}</p>
+
+	<h2>posting speed</h2>
+		<p>	{{post.user.created_at}}</p>
+
+	<h2> Favorite </h2>
+		<ul v-for="fav_user in post.favorited_users" v-bind:key="fav_user.id">
+			img : {{fav_user.avatar_url}}  username : {{fav_user.username}}
+		</ul>
+
+
+<!--  dont have to edit why of this is a seen_users datas-->
+<!-- <p v-for="user in post.seen_users" v-bind:key="user.email">
+	{{user.created_at}}
+</p> -->
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
 import { Post } from '~/schemas'
+
 
 @Component({})
 export default class PostDetail extends Vue {
